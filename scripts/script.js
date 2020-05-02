ae77f1ee0e8d897abfd46fc27d2fe42f645aa548
Aakash Adesara <aakash.adesara@gmail.com>
2020-05-01 20:44:54 -0700

    classes + filters + firebase

---
+++ scripts/script.js
@@ -0,0 +1,186 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAl7n-491RillgKM26yYTY_7C3zjLXU2pE",
+    authDomain: "quarantine4kids-5cfbf.firebaseapp.com",
+    databaseURL: "https://quarantine4kids-5cfbf.firebaseio.com",
+    projectId: "quarantine4kids-5cfbf",
+    storageBucket: "quarantine4kids-5cfbf.appspot.com",
+    messagingSenderId: "820598508139",
+    appId: "1:820598508139:web:a92ceb278e1020296abf2e",
+    measurementId: "G-R7WWB7Z319"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.database();
+
+$( document ).ready(function() {
+
+    getAllClasses();
+
+});
+
+const colors = [
+    "#FC515C",
+    "#4C58E0",
+    "#27AAE2",
+    "#555"
+];
+
+function getAllClasses() {
+    db.ref('/classes').once('value').then(function(snapshot) {
+        const data = snapshot.val();
+        const keys = Object.keys(data);
+
+        var workshopList = [];
+        for (var i = 0; i < keys.length; i++) {
+            workshopList.push(data[keys[i]]);
+        }
+
+        addWorkshops(workshopList)
+      });
+}
+
+function addWorkshops(workshopList) {
+    var count = 0;
+    workshopList.forEach(function(workshop) {
+
+        addClass(workshop, colors[count % 4]);
+        count++;
+
+    });
+}
+
+function addStartRow() {
+    let addition = '<div class="row">';
+    $("#classContainer").html($("#classContainer").html() + addition);
+}
+
+function addEndRow() {
+    let addition = '</div>';
+    $("#classContainer").html($("#classContainer").html() + addition);
+}
+
+function addClass(workshop, color) {
+    const photo = workshop.photo;
+    const name = workshop.name;
+    const link = workshop.link;
+    const descr = workshop.descr;
+    const age = workshop.age;
+    const type = workshop.type;
+
+    
+    var addition = '<div class="col-md-4" style="float: left;">' + 
+                        '<div class="class-modal" style="padding: 20px; border-bottom: solid 10px ' + color + ';">' +
+                            '<img class="class-modal-img" src="' + 
+                            photo + 
+                            '"/>' + 
+                            '<div style="width: 100%; height: 10px; background-color: ' + color + ';"></div>' +
+                            '<div class="row">' +
+                                '<div class="col-md-12" style="padding: 15px !important;">' +
+                                    '<p style="font-weight: bold; font-size: large; margin: 0px; margin-left: 15px;">' +
+                                        name +
+                                    '</p>' +
+                                    '<p style="font-weight: bold; font-size: medium; margin: 0px; color: #777; margin-left: 15px;">Ages ' +
+                                        age + ' (' + type + ')' +
+                                    '</p>' +
+                                '</div>' + 
+                            '</div>' +
+                            '<div>' +
+                                '<p style="font-weight: bold; font-size: smaller; margin: 0px; margin-left: 15px; margin-right: 15px; padding-bottom: 20px;">' +
+                                    descr +
+                                '</p>' +
+                                '<p style="color: ' + color + '; margin: 0px; text-align: center;" class="col-md-12">100% Free</p>' +
+                                    '<button class="col-md-8 offset-md-2 signupbutton white" style="background-color: ' + color + ';" href="' + 
+                                        link +
+                                    '">Sign Up</button><br><br>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>';
+    $("#classContainer").html($("#classContainer").html() + addition);
+}
+
+// Filter Methods
+function getFilteredClasses(filter) {
+    db.ref('/classes').once('value').then(function(snapshot) {
+        const data = snapshot.val();
+        const keys = Object.keys(data);
+
+        var workshopList = [];
+        for (var i = 0; i < keys.length; i++) {
+            var obj = data[keys[i]];
+
+            if (obj.type === filter) {
+                workshopList.push(obj);
+            }
+        }
+
+        addWorkshops(workshopList)
+      });
+}
+
+function getAgeClasses(filter) {
+    db.ref('/classes').once('value').then(function(snapshot) {
+        const data = snapshot.val();
+        const keys = Object.keys(data);
+
+        var workshopList = [];
+        for (var i = 0; i < keys.length; i++) {
+            var obj = data[keys[i]];
+
+            if (obj.age === filter) {
+                workshopList.push(obj);
+            }
+        }
+
+        addWorkshops(workshopList)
+      });
+}
+
+function allClasses() {
+    $("#classContainer").html("");
+    $("#classTitle").html("All Classes")
+    getAllClasses();
+}
+
+function drawingClasses() {
+    $("#classContainer").html("");
+    $("#classTitle").html("Drawing")
+    getFilteredClasses("drawing");
+}
+
+function readingClasses() {
+    $("#classContainer").html("");
+    $("#classTitle").html("Reading")
+    getFilteredClasses("reading");
+}
+
+function craftClasses() {
+    $("#classContainer").html("");
+    $("#classTitle").html("Craft")
+    getFilteredClasses("craft");
+}
+
+function ages3_6() {
+    $("#classContainer").html("");
+    $("#classTitle").html("Ages 3-6")
+    getAgeClasses("3-6");
+}
+
+function ages6_9() {
+    $("#classContainer").html("");
+    $("#classTitle").html("Ages 6-9")
+    getAgeClasses("6-9");
+}
+
+function ages8_12() {
+    $("#classContainer").html("");
+    $("#classTitle").html("Ages 9-12")
+    getAgeClasses("9-12");
+}
+
+
+function ages12_15() {
+    $("#classContainer").html("");
+    $("#classTitle").html("Ages 12-15")
+    getAgeClasses("12-15");
+}
